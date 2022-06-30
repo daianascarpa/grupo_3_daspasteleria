4e754bb5d3bd7df4417eb814c62b46d4a5e7054a
unknown <carinnovello@gmail.com>
2022-06-29 23:39:02 -0300

    creando metodos en productControllers en clase

--- dasPasteleria/src/controllers/productController.js
+++ dasPasteleria/src/controllers/productController.js
@@ -1,7 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath =  path.join(__dirname, '../data/dasProductList.json');
+const product = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+
 const productController = {
     product : function(req,res){
-        res.render('product',{titulo: "Productos"})
-    }
+        res.render('product',)
+    },
+
+    Detail : function(req,res){
+        res.render('productView',{/*titulo:  Nombre del producto*/})
+},
+
+    create : function(req,res){
+            res.render('createProd', {titulo: "Formulario de Creación"})
+    },
+
+    editar : function(req,res){
+            res.redirect('/product', {/*titulo:  Nombre del producto*/})
+    },
+
+    delete : function(req,res){
+            res.redirect('/product', {/*titulo:  Nombre del producto*/})
+
 }
+}
+
+
+
+
+
 
 module.exports = productController
