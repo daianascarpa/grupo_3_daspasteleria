5b3f156f3d1deec384d20174678bdec87736185c
thomasscruzz <thomascruz699@gmail.com>
2022-07-19 16:43:07 -0300

    modificacion de validaciones del register

--- dasPasteleria/src/routes/userRouter.js
+++ dasPasteleria/src/routes/userRouter.js
@@ -5,14 +5,15 @@ const userController = require('../controllers/userController')
 
 const validacionFormularioLogin = [
     body('email').notEmpty().isEmail().withMessage('campo obligatorio'), // campo obligatorio de completar, de tipo email y con mensaje al usuario en caso de error
-    body('password').notEmpty().isLength({ min:8} ).withMessage('campo obligatorio') // campo obligatorio de completar y con mensaje .
+    body('password').notEmpty().withMessage('campo obligatorio') // campo obligatorio de completar y con mensaje .
 ];
 
 const validationRegisterForm = [
     check('email').notEmpty().isEmail().withMessage('campo obligatorio'), 
-    check('password').notEmpty().isLength({ min:8} ).withMessage('campo obligatorio'),
-    check('passwordRepeat').notEmpty().isLength({ min:8} ).withMessage('campo obligatorio'),
-    check('name').notEmpty().withMessage('campo obligatorio')
+    check('password').notEmpty().withMessage('campo obligatorio'),
+    check('passwordRepeat').notEmpty().withMessage('campo obligatorio'),
+    check('name').notEmpty().withMessage('campo obligatorio'),
+    check('aceptoTerminosCondiciones').withMessage('campo obligatorio') // crear un campo obligatorio para que si o si se tenga que clickear el checkbox
 ];
 
 router.get('/login',userController.login) // mostrar formulario de login
